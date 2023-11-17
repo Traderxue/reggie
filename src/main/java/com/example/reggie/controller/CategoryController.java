0f10d353d3b2ea9365d7e15c0c9aa42c8ff34256
mr xue <falethe@gmail.com>
2023-11-17 12:17:01 +0800

    init

--- src/main/java/com/example/reggie/controller/CategoryController.java
+++ src/main/java/com/example/reggie/controller/CategoryController.java
@@ -1,12 +1,12 @@
 package com.example.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.reggie.common.R;
 import com.example.reggie.entity.Category;
 import com.example.reggie.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/category")
@@ -16,8 +16,26 @@ public class CategoryController {
     private CategoryService categoryService;
 
 
+    @PostMapping
     public R<String> save(@RequestBody Category category){
         categoryService.save(category);
         return R.success("新增分类成功");
+    }
+
+    @GetMapping("/page")
+    public R<Page> page(int page,int pageSize){
+        Page<Category> pageInfo = new Page<>(page,pageSize);
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.orderByAsc(Category::getSort);
+
+        categoryService.page(pageInfo,queryWrapper);
+
+        return R.success(pageInfo);
+    }
+
+    @DeleteMapping
+    public R<String> delete(Long id){
+        categoryService.removeById(id);
+        return R.success("删除成功");
     }
 }
